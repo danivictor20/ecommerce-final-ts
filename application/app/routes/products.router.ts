c244334acb9a21801b425a14036aa3067160e9fa
Daniel Victor <danielvictorgomez@gmail.com>
2022-07-13 12:08:30 -0300

    nueva versión

--- application/app/routes/products.router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-
-import productsController from '../controllers/products.controller'
-
-const router:Router = Router()
-
-router.get('/', productsController.all)
-
-export default router
--- application/app/utils/logger.util.ts
+++
@@ -1,8 +0,0 @@
-import winston from 'winston'
-
-export const logger = winston.createLogger({
-    level: 'silly',
-    transports: [
-        new winston.transports.Console({ level: 'silly' })
-    ]
-})
